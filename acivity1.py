116620c434dd29af087bd95548feef50ce85546d
maneetHTML <maneet.harmeet@gmail.com>
2025-02-11 17:09:53 +1300

    class 7

---
+++ acivity1.py
@@ -0,0 +1,12 @@
+# a=7
+# b=8
+# if a is b:
+#     print("it is the same type")
+# else:
+#     print("it is not the same")
+word = input("enter your word:")
+letter = input("enter your letter:")
+if letter in word:
+    print(letter,"letter in word")
+else:
+    print(letter,"letter is not in the word")
---
+++ activity2.py
@@ -0,0 +1,5 @@
+# a = int(input("enter your number:"))
+# print("binary value", bin(a).replace("0b",""))
+a=55
+print(a<<1)
+print(a>>1)
